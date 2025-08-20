26b7dd6b75f44ec141bd00cbd288adf3b4b208c2
barsdeveloper <barsdeveloper@gmail.com>
2025-08-20 22:54:47 +0300

    Reorganize tests

--- tank-tests/src/lib.rs
+++ tank-tests/src/lib.rs
@@ -2,15 +2,17 @@
 #![feature(box_patterns)]
 mod aggregates;
 mod books;
+mod complex;
 mod insane;
-mod nullability;
+mod simple;
 mod trade;
 mod user;
 
 use crate::{
     books::books,
+    complex::complex,
     insane::insane,
-    nullability::{complex_null_fields, single_null_fields},
+    simple::simple,
     trade::{trade_multiple, trade_simple},
     user::users,
 };
@@ -18,12 +20,12 @@ use aggregates::aggregates;
 use tank::Connection;
 
 pub async fn execute_tests<C: Connection>(connection: &mut C) {
-    single_null_fields(connection).await;
+    simple(connection).await;
     trade_simple(connection).await;
     trade_multiple(connection).await;
     aggregates(connection).await;
     users(connection).await;
     books(connection).await;
-    complex_null_fields(connection).await;
+    complex(connection).await;
     insane(connection).await;
 }
---
+++ tank-tests/src/simple.rs
@@ -0,0 +1,103 @@
+use std::{borrow::Cow, sync::LazyLock};
+use tank::{Connection, Entity};
+use time::Time;
+use tokio::sync::Mutex;
+use uuid::Uuid;
+
+pub async fn simple<C: Connection>(connection: &mut C) {
+    #[derive(Entity)]
+    struct SimpleFields {
+        alpha: Option<u8>,
+        bravo: Option<i32>,
+        charlie: Option<i16>,
+        delta: Option<u64>,
+        echo: Option<Uuid>,
+        foxtrot: Option<i128>,
+        golf: Option<Time>,
+        hotel: Option<Cow<'static, str>>,
+        india: Box<Option<char>>,
+    }
+
+    static MUTEX: LazyLock<Mutex<()>> = LazyLock::new(|| Mutex::new(()));
+    let _lock = MUTEX.lock().await;
+
+    // Setup
+    SimpleFields::drop_table(connection, true, false)
+        .await
+        .expect("Failed to drop SimpleNullFields table");
+    SimpleFields::create_table(connection, true, true)
+        .await
+        .expect("Failed to create SimpleNullFields table");
+
+    // Simple 1
+    SimpleFields::delete_many(connection, &true)
+        .await
+        .expect("Failed to clear the SimpleNullFields table");
+    let entity = SimpleFields {
+        alpha: None,
+        bravo: 777.into(),
+        charlie: (-2).into(),
+        delta: 9876543210.into(),
+        echo: None,
+        foxtrot: i128::MAX.into(),
+        golf: Time::from_hms(12, 0, 10).unwrap().into(),
+        hotel: Some("Hello world!".into()),
+        india: Box::new(None),
+    };
+    entity
+        .save(connection)
+        .await
+        .expect("Failed to save simple 1");
+    let entity = SimpleFields::find_one(connection, &true)
+        .await
+        .expect("Failed to query simple 1")
+        .expect("Failed to find simple 1");
+    assert_eq!(entity.alpha, None);
+    assert_eq!(entity.bravo, Some(777));
+    assert_eq!(entity.charlie, Some(-2));
+    assert_eq!(entity.delta, Some(9876543210));
+    assert_eq!(entity.echo, None);
+    assert_eq!(
+        entity.foxtrot,
+        Some(170_141_183_460_469_231_731_687_303_715_884_105_727)
+    );
+    assert_eq!(entity.golf, Some(Time::from_hms(12, 0, 10).unwrap()));
+    assert_eq!(entity.hotel, Some("Hello world!".into()));
+    assert_eq!(*entity.india, None);
+
+    // Simple 2
+    SimpleFields::delete_many(connection, &true)
+        .await
+        .expect("Failed to clear the SimpleNullFields table");
+    let entity = SimpleFields {
+        alpha: 255.into(),
+        bravo: None,
+        charlie: None,
+        delta: None,
+        echo: Some(Uuid::parse_str("5e915574-bb30-4430-98cf-c5854f61fbbd").unwrap()),
+        foxtrot: None,
+        golf: None,
+        hotel: None,
+        india: Box::new(None),
+    };
+    entity
+        .save(connection)
+        .await
+        .expect("Failed to save simple 2");
+    let entity = SimpleFields::find_one(connection, &true)
+        .await
+        .expect("Failed to query simple 2")
+        .expect("Failed to find simple 2");
+    assert_eq!(entity.alpha, Some(255));
+    assert_eq!(entity.bravo, None);
+    assert_eq!(entity.charlie, None);
+    assert_eq!(entity.delta, None);
+    assert_eq!(
+        entity.echo,
+        Some(Uuid::parse_str("5e915574-bb30-4430-98cf-c5854f61fbbd").unwrap())
+    );
+    assert_eq!(entity.foxtrot, None);
+    assert_eq!(entity.golf, None);
+    assert_eq!(entity.hotel, None);
+    assert_eq!(*entity.india, None);
+}
