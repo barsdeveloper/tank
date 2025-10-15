03696b650b78cf91ad5ba5ef1d0193ed567d093b
barsdeveloper <barsdeveloper@gmail.com>
2025-10-15 19:14:06 +0300

    Publishing script

---
+++ publish.sh
@@ -0,0 +1,36 @@
+cd tank-core/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-macros/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-tests/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-duckdb/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-postgres/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-sqlite/ &&
+cargo publish --dry-run &&
+cd ../ &&
+cd tank-core/ &&
+cargo publish &&
+cd ../ &&
+cd tank-macros/ &&
+cargo publish &&
+cd ../ &&
+cd tank-tests/ &&
+cargo publish &&
+cd ../ &&
+cd tank-duckdb/ &&
+cargo publish &&
+cd ../ &&
+cd tank-postgres/ &&
+cargo publish &&
+cd ../ &&
+cd tank-sqlite/ &&
+cargo publish &&
+cd ../
